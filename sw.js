ca2eb0d0451db97343cf405d98cd58686a8e5bc4
BTCchartist <chartist7674@gmail.com>
2026-06-27 18:11:06 +0000

    SW: network-first로 변경, 캐시 버전 v3

--- sw.js
+++ sw.js
@@ -1,19 +1,23 @@
-const CACHE_NAME = 'chartist-v1';
-const urlsToCache = [
-  '/',
-  '/manifest.json'
-];
+const CACHE_NAME = 'chartist-v3';
+const urlsToCache = ['/manifest.json'];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
